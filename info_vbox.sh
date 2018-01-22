99cd7550f69eacdabb7379e9edfec90404b892e9
ZoranStojsavljevic <zoran.stojsavljevic@gmail.com>
2018-01-22 20:22:04 +0100

    Creating new bash script which is essential for giving the info about Virtual Box VM and vagrant relationship with the same VM via VBox
    Creating new bash script which is essential for giving the info about Virtual Box VM and vagrant relationship with the same VM via VBox

---
+++ info_vbox.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Copyright (C) 2018, Siemens AG, Zoran Stojsavljevic
+# SPDX-License-Identifier:	AGPL-3.0
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, version 3.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+
+## Print out all the relevant data for the vagrant machine and underlying Virtual Box
+## Find Virtual Box VM machine directory
+VBOX_IMG_DIR=`vboxmanage list systemproperties | grep "Default machine folder" | cut -b 34-`
+echo "Virtual Box VM machine directory: $VBOX_IMG_DIR"
+
+if [ ! -d ".vagrant" ]; then
+    echo "vagrant machine not created"
+    exit 1
+fi
+
+## Find the UUID of the currently used Virtual Box machine
+vbox_uuid=`cat .vagrant/machines/default/virtualbox/id`
+echo "The UUID of the currently used Virtual Box machine $vbox_uuid"
+
+## Find the UUID of the currently used vagrant machine
+vagrant_uuid=`cat .vagrant/machines/default/virtualbox/index_uuid`
+echo "The UUID of the currently used vagrant machine $vagrant_uuid"
+
+echo "What vagrant machines are active in the system?"
+vagrant box list
+
+echo "What Virtual Box machines are active in the system?"
+vboxmanage list vms
+
+echo "Do we want to destroy vagrant machine, affecting/destroying Virtual Box VM as well?"
+vagrant destroy $vagrant_uuid
