41d9806bdcb69d389a57d3b3d8349a984b7668c4
ZoranStojsavljevic <zoran.stojsavljevic@gmail.com>
2018-02-06 16:59:24 +0100

    Added in the private-net directory the potential candidates for the vagrant secondary rsync
    - The real candidate is conf_private_net.sh, which is cloned test.sh
    - test.sh.dhcpd is the previous FAILED version of dhcpd service, with lot of useful stuff in the script
    - test.sh.dnsmasq is former version of the test.sh -> conf_private_net.sh
    - Another used files:
    [1] /etc/dnsmasq.conf
    [2] /etc/systemd/network/static.network
    [3] /etc/default/tftpd-hpa
    [4] /etc/egtab

--- private-net/test.sh
+++ private-net/test.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-# Copyright (C) 2018, Siemens AG, Zoran Stojsavljevic
-# SPDX-License-Identifier:	AGPL-3.0
-# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
-
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-echo "START: conf_private_net.sh"
-
-## Configure network interfaces
-## sudo copy ./interfaces /etc/network/interfaces
-## sudo apt-get install ndiswrapper ndiswrapper-dkms ndiswrapper-source
-
-## Put some additional useful packages into VM
-## sudo apt-get install usbutils usbip-utils
-
-#!/bin/bash
 
 echo "START test script: test.sh"
 
